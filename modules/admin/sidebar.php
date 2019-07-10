374a19a0dfa746cf17830d94f5d05c657b908565
anilkumar9989 <51983070+anilkumar9989@users.noreply.github.com>
2019-07-10 13:05:11 +0530

    pz

--- modules/admin/sidebar.php
+++ modules/admin/sidebar.php
@@ -8,6 +8,12 @@
 			<div class="sidebar-menu">
 				<ul id="accordion-menu">
 					<li class="dropdown">
+						<a href="dashboard.php" class="dropdown-toggle">
+							<span class="fa fa-dashboard"></span><span class="mtext">Dashboard </span>
+						</a>
+						
+					</li>
+					<li class="dropdown">
 						<a href="javascript:;" class="dropdown-toggle">
 							<span class="fa fa-home"></span><span class="mtext">Restaurant </span>
 						</a>
@@ -16,7 +22,24 @@
 							<li><a href="restaurant-list.php">Dashboard style 2</a></li>
 						</ul>
 					</li>
-					
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Payments</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="payment-details.php">Add Payments</a></li>
+							
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Running Client Status</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="running-client-status.php">Add Running Client Status</a></li>
+							
+						</ul>
+					</li>
 				</ul>
 			</div>
 		</div>
