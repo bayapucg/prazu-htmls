df96ec89a84021ff56135db0b463c17c6198b925
bayapucg <bayapu.edu004@gmail.com>
2019-07-10 16:03:04 +0530

    commit
    cahngs

---
+++ modules/sub-admin/sidebar.php
@@ -0,0 +1,47 @@
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="../vendors/images/deskapp-logo.png" alt="">
+			</a>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li class="dropdown">
+						<a href="dashboard.php" class="dropdown-toggle">
+							<span class="fa fa-dashboard"></span><span class="mtext">Dashboard </span>
+						</a>
+					
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Restaurant </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-restaurant.php">Add Restaurant</a></li>
+							<li><a href="restaurant-list.php">Restaurant List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Tables </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-table.php">Add table</a></li>
+							<li><a href="tables-list.php">Tables List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Offers & deals </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="offers-deals.php">Offers & deals</a></li>
+							<li><a href="tables-list.php">Offers & Deals List</a></li>
+						</ul>
+					</li>
+					
+				</ul>
+			</div>
+		</div>
+	</div>
