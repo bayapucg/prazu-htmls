c3272c1a28dac3c7678bd945f5742783db4b6fd0
bayapucg <bayapu.edu004@gmail.com>
2019-07-10 11:52:23 +0530

    commit

--- modules/admin/add-restaurant.php
+++ modules/admin/add-restaurant.php
@@ -1,24 +1,7 @@
 <?php include( "head.php"); ?>
 <?php include( "header.php"); ?>
 <?php include( "sidebar.php"); ?>
-<style>
- fieldset.scheduler-border {
-    border: 1px groove #ddd !important;
-    padding: 0 1.4em 1.4em 1.4em !important;
-    margin: 0 0 1.5em 0 !important;
-    -webkit-box-shadow:  0px 0px 0px 0px #000;
-            box-shadow:  0px 0px 0px 0px #000;
-}
 
-    legend.scheduler-border {
-        font-size: 1.2em !important;
-        font-weight: bold !important;
-        text-align: left !important;
-        width:auto;
-        padding:0 10px;
-        border-bottom:none;
-    }
-</style>
 <div class="main-container">
 	<div class="pd-ltr-20 customscroll customscroll-10-p height-100-p xs-pd-20-10">
 		<div class="bg-white pd-10 box-shadow border-radius-5 mb-10">
@@ -54,13 +37,13 @@
 						</div>
 					</div>
 					<div class="form-group col-md-6">
-						<label class="">Restaurant Address</label>
+						<label class="">Restaurant Mobile</label>
 						<div class="">
 							<input type="text" class="form-control" name="firstName" placeholder="Enter Restaurant Address" />
 						</div>
 					</div>
 					<div class="form-group col-md-6">
-						<label class="">Restaurant Mobile Number</label>
+						<label class="">Email</label>
 						<div class="">
 							<input type="text" class="form-control" name="firstName" placeholder="Restaurant Mobile Number" />
 						</div>
