6a8c1a80bc151cf364090d1e9dcbc745bac75ac4
bayapucg <bayapu.edu004@gmail.com>
2019-07-11 16:04:36 +0530

    fdsfdsf
    fdsafdsf

---
+++ modules/Kitchen/sidebar.php
@@ -0,0 +1,93 @@
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="../vendors/images/deskapp-logo.png" alt="">
+			</a>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li class="dropdown">
+						<a href="dashboard.php" class="dropdown-toggle">
+							<span class="fa fa-dashboard"></span><span class="mtext">Dashboard </span>
+						</a>
+					
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Restaurant </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-restaurant.php">Add Restaurant</a></li>
+							<li><a href="restaurant-list.php">Restaurant List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Tables </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-table.php">Add table</a></li>
+							<li><a href="tables-list.php">Tables List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Offers & deals </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="offers-deals.php">Offers & deals</a></li>
+							<li><a href="offers-list.php">Offers & Deals List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Menu</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-menu.php">Add Menu</a></li>
+							<li><a href="tables-list.php">Menu List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Billing</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-billing.php">Add Billing</a></li>
+							<li><a href="tables-list.php">Billing List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Feedback & Ratings </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-billing.php">Add Feedback & Ratings</a></li>
+							<li><a href="tables-list.php">Menu List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Kitchen Module</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-billing.php">Add Kitchen Module</a></li>
+							<li><a href="tables-list.php">Menu List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Menu Module </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-billing.php">Add Menu Module</a></li>
+							<li><a href="tables-list.php">Menu List</a></li>
+						</ul>
+					</li>
+					
+					
+				</ul>
+			</div>
+		</div>
+	</div>
