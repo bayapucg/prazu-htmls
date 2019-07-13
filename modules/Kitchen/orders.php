9e2b4f805af4db5416314a84cd782a39c26a0df1
bayapucg <bayapu.edu004@gmail.com>
2019-07-13 12:39:50 +0530

    commit
    cahnges

--- modules/Kitchen/orders.php
+++ modules/Kitchen/orders.php
@@ -40,115 +40,316 @@
 				
 				<!-- multiple select row Datatable End -->
 				<!-- Export Datatable start -->
-				<div class="pd-20 bg-white border-radius-4 box-shadow mb-30">
+				<div class="pd-20 border-radius-4 box-shadow mb-30">
 				
 					<div class="row">
-						<table class="stripe data-table">
-							<thead>
-								<tr>
-									<th class="">Sr.no</th>
-									<th>Floor No</th>
-									<th class="table-plus datatable-nosort">Table No</th>
-									<th>Dish</th>
-									<th>Quantity</th>
-									<th>Price</th>
-									<th>Received Time</th>
-									<th >Update Order</th>
-									<th >Close Order</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td class="1">1</td>
-									<td>1</td>
-									<td class="table-plus">2</td>
-									<td>Butter chicken</td>
-									<td>1</td>
-									<td>200</td>
-									<td>12:00PM</td>
-									<td> 
-										<button type="button" class="btn btn-success btn-sm">Update Order</button>
-										
-									</td>
-									<td> 
-										<button type="button" class="btn btn-danger btn-sm">Close Order</button>
-										
-									</td>
-											
-								</tr>
+				<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+					<div class="bg-white pd-20 box-shadow border-radius-5 height-100-p pos-reltive">
+						<div class="project-info clearfix">
+								<div class="project-info-left">
+								<div class="icon box-shadow  text-white">
+									<img src="../vendors/images/table.png">
+								</div>
+							</div>
+							<div class="project-info-right" style="text-align:left;margin-top:20px; ">
+								<span class="no text-blue weight-500 font-24">Table :4</span>
 							
-								
-							</tbody>
-						</table>
+							</div>
+							<div style="position: absolute;right:-10px;top:-10px;">
+								<div style="background:#fff;height:40px;width:40px;border-radius:50%;padding:5px;-webkit-box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);text-align:center">
+									5
+								</div>
+							</div>
+							
+						</div>
+						<hr style="margin:0px 0px 5px 0px;">
+						<div class="">
+							<a href="">Amt :3500</a> | 
+							<a href="" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#bd-example-modal-lg" >View</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+					<div class="bg-white pd-20 box-shadow border-radius-5 height-100-p pos-reltive">
+						<div class="project-info clearfix">
+								<div class="project-info-left">
+								<div class="icon box-shadow  text-white">
+									<img src="../vendors/images/table.png">
+								</div>
+							</div>
+							<div class="project-info-right" style="text-align:left;margin-top:20px; ">
+								<span class="no text-blue weight-500 font-24">Table :4</span>
+							
+							</div>
+							<div style="position: absolute;right:-10px;top:-10px;">
+								<div style="background:#fff;height:40px;width:40px;border-radius:50%;padding:5px;-webkit-box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);text-align:center">
+									5
+								</div>
+							</div>
+							
+						</div>
+						<hr style="margin:0px 0px 5px 0px;">
+						<div class="">
+							<a href="">Amt :3500</a> | 
+							<a href="" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#bd-example-modal-lg" >View</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+					<div class="bg-white pd-20 box-shadow border-radius-5 height-100-p pos-reltive">
+						<div class="project-info clearfix">
+								<div class="project-info-left">
+								<div class="icon box-shadow  text-white">
+									<img src="../vendors/images/table.png">
+								</div>
+							</div>
+							<div class="project-info-right" style="text-align:left;margin-top:20px; ">
+								<span class="no text-blue weight-500 font-24">Table :4</span>
+							
+							</div>
+							<div style="position: absolute;right:-10px;top:-10px;">
+								<div style="background:#fff;height:40px;width:40px;border-radius:50%;padding:5px;-webkit-box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);text-align:center">
+									5
+								</div>
+							</div>
+							
+						</div>
+						<hr style="margin:0px 0px 5px 0px;">
+						<div class="">
+							<a href="">Amt :3500</a> | 
+							<a href="" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#bd-example-modal-lg" >View</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+					<div class="bg-white pd-20 box-shadow border-radius-5 height-100-p pos-reltive">
+						<div class="project-info clearfix">
+								<div class="project-info-left">
+								<div class="icon box-shadow  text-white">
+									<img src="../vendors/images/table.png">
+								</div>
+							</div>
+							<div class="project-info-right" style="text-align:left;margin-top:20px; ">
+								<span class="no text-blue weight-500 font-24">Table :4</span>
+							
+							</div>
+							<div style="position: absolute;right:-10px;top:-10px;">
+								<div style="background:#fff;height:40px;width:40px;border-radius:50%;padding:5px;-webkit-box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);text-align:center">
+									5
+								</div>
+							</div>
+							
+						</div>
+						<hr style="margin:0px 0px 5px 0px;">
+						<div class="">
+							<a href="">Amt :3500</a> | 
+							<a href="" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#bd-example-modal-lg" >View</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+					<div class="bg-white pd-20 box-shadow border-radius-5 height-100-p pos-reltive">
+						<div class="project-info clearfix">
+								<div class="project-info-left">
+								<div class="icon box-shadow  text-white">
+									<img src="../vendors/images/table.png">
+								</div>
+							</div>
+							<div class="project-info-right" style="text-align:left;margin-top:20px; ">
+								<span class="no text-blue weight-500 font-24">Table :4</span>
+							
+							</div>
+							<div style="position: absolute;right:-10px;top:-10px;">
+								<div style="background:#fff;height:40px;width:40px;border-radius:50%;padding:5px;-webkit-box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);text-align:center">
+									5
+								</div>
+							</div>
+							
+						</div>
+						<hr style="margin:0px 0px 5px 0px;">
+						<div class="">
+							<a href="">Amt :3500</a> | 
+							<a href="" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#bd-example-modal-lg" >View</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+					<div class="bg-white pd-20 box-shadow border-radius-5 height-100-p pos-reltive">
+						<div class="project-info clearfix">
+								<div class="project-info-left">
+								<div class="icon box-shadow  text-white">
+									<img src="../vendors/images/table.png">
+								</div>
+							</div>
+							<div class="project-info-right" style="text-align:left;margin-top:20px; ">
+								<span class="no text-blue weight-500 font-24">Table :4</span>
+							
+							</div>
+							<div style="position: absolute;right:-10px;top:-10px;">
+								<div style="background:#fff;height:40px;width:40px;border-radius:50%;padding:5px;-webkit-box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);text-align:center">
+									5
+								</div>
+							</div>
+							
+						</div>
+						<hr style="margin:0px 0px 5px 0px;">
+						<div class="">
+							<a href="">Amt :3500</a> | 
+							<a href="" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#bd-example-modal-lg" >View</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+					<div class="bg-white pd-20 box-shadow border-radius-5 height-100-p pos-reltive">
+						<div class="project-info clearfix">
+								<div class="project-info-left">
+								<div class="icon box-shadow  text-white">
+									<img src="../vendors/images/table.png">
+								</div>
+							</div>
+							<div class="project-info-right" style="text-align:left;margin-top:20px; ">
+								<span class="no text-blue weight-500 font-24">Table :4</span>
+							
+							</div>
+							<div style="position: absolute;right:-10px;top:-10px;">
+								<div style="background:#fff;height:40px;width:40px;border-radius:50%;padding:5px;-webkit-box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);text-align:center">
+									5
+								</div>
+							</div>
+							
+						</div>
+						<hr style="margin:0px 0px 5px 0px;">
+						<div class="">
+							<a href="">Amt :3500</a> | 
+							<a href="" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#bd-example-modal-lg" >View</a>
+						</div>
+					</div>
+				</div>
+				
+					
 					</div>
 				</div>
 				<!-- Export Datatable End -->
 			</div>
+			<div class="modal fade bs-example-modal-lg" id="bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+								<div class="modal-dialog modal-lg modal-dialog-centered">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h4 class="modal-title" id="myLargeModalLabel">Table 4 Menu Details</h4>
+											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+										</div>
+										<div class="modal-body">
+											<table class="table table-bordered">
+											  <thead>
+												<tr>
+												  <th scope="col">#</th>
+												  <th scope="col">Item Name</th>
+												  <th scope="col">Item Type</th>
+												  <th scope="col">Qty</th>
+												  <th scope="col">Price</th>
+												  <th scope="col">Status</th>
+												</tr>
+											  </thead>
+											  <tbody>
+												<tr>
+												  <td >1</td>
+												  <td>Lollypup </td>
+												  <td>Non Veg </td>
+												  <td>2 </td>
+												  <td>350  </td>
+												  <td>
+													<a href="" class="btn btn-success btn-sm"><i class="fa fa-check-circle-o" aria-hidden="true"></i> Delivered</a>
+													
+												   </td>
+												  </td>
+												</tr>
+													<tr>
+												  <td >2</td>
+												  <td>Lollypup </td>
+												  <td>Non Veg </td>
+												  <td>2 </td>
+												  <td>350  </td>
+												  <td>
+													<a href="" class="btn btn-primary btn-sm">Update</a>
+													<a href="" class="btn btn-danger btn-sm">Cancel</a>
+												   </td>
+												  </td>
+												</tr>
+													<tr>
+												  <td >3</td>
+												  <td>Lollypup </td>
+												  <td>Non Veg </td>
+												  <td>2 </td>
+												  <td>350  </td>
+												  <td>
+													<a href="" class="btn btn-primary btn-sm">Update</a>
+													<a href="" class="btn btn-danger btn-sm">Cancel</a>
+												   </td>
+												  </td>
+												</tr>
+												<tr>
+												  <td >4</td>
+												  <td>Lollypup </td>
+												  <td>Non Veg </td>
+												  <td>2 </td>
+												  <td>350  </td>
+												  <td>
+													<a href="" class="btn btn-primary btn-sm">Update</a>
+													<a href="" class="btn btn-danger btn-sm">Cancel</a>
+												   </td>
+												  </td>
+												</tr>
+												<tr>
+												  <td >4</td>
+												  <td>Lollypup </td>
+												  <td>Non Veg </td>
+												  <td>2 </td>
+												  <td>350  </td>
+												  <td>
+													<a href="" class="btn btn-primary btn-sm">Update</a>
+													<a href="" class="btn btn-danger btn-sm">Cancel</a>
+												   </td>
+												  </td>
+												</tr>
+												<tr>
+												  <td >4</td>
+												  <td>Lollypup </td>
+												  <td>Non Veg </td>
+												  <td>2 </td>
+												  <td>350  </td>
+												  <td>
+													<a href="" class="btn btn-primary btn-sm">Update</a>
+													<a href="" class="btn btn-danger btn-sm">Cancel</a>
+												   </td>
+												  </td>
+												</tr>
+												<tr>
+													<th colspan="2" >
+														<div class="">Tax: 150</div>
+													</th>
+													<th colspan="2" >
+														<div class="">GST: 150</div>
+												
+													</th>
+													<th colspan="2" >
+													
+														<div class="">Grand Total: 1400</div>
+													</th>
+												</tr>
+											  </tbody>
+											</table>
+										</div>
+										<div class="modal-footer">
+											
+											<button type="button" class="btn btn-primary">Close Order</button>
+										</div>
+									</div>
+								</div>
+							</div>
 			<?php include('footer.php'); ?>
 		</div>
 	</div>
 	<?php include('script.php'); ?>
-	<script src="../src/plugins/datatables/media/js/jquery.dataTables.min.js"></script>
-	<script src="../src/plugins/datatables/media/js/dataTables.bootstrap4.js"></script>
-	<script src="../src/plugins/datatables/media/js/dataTables.responsive.js"></script>
-	<script src="../src/plugins/datatables/media/js/responsive.bootstrap4.js"></script>
-	<!-- buttons for Export datatable -->
-	<script src="../src/plugins/datatables/media/js/button/dataTables.buttons.js"></script>
-	<script src="../src/plugins/datatables/media/js/button/buttons.bootstrap4.js"></script>
-	<script src="../src/plugins/datatables/media/js/button/buttons.print.js"></script>
-	<script src="../src/plugins/datatables/media/js/button/buttons.html5.js"></script>
-	<script src="../src/plugins/datatables/media/js/button/buttons.flash.js"></script>
-	<script src="../src/plugins/datatables/media/js/button/pdfmake.min.js"></script>
-	<script src="../src/plugins/datatables/media/js/button/vfs_fonts.js"></script>
-	<script>
-		$('document').ready(function(){
-			$('.data-table').DataTable({
-				scrollCollapse: true,
-				autoWidth: false,
-				responsive: true,
-				columnDefs: [{
-					targets: "datatable-nosort",
-					orderable: false,
-				}],
-				"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-				"language": {
-					"info": "_START_-_END_ of _TOTAL_ entries",
-					searchPlaceholder: "Search"
-				},
-			});
-			$('.data-table-export').DataTable({
-				scrollCollapse: true,
-				autoWidth: false,
-				responsive: true,
-				columnDefs: [{
-					targets: "datatable-nosort",
-					orderable: false,
-				}],
-				"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-				"language": {
-					"info": "_START_-_END_ of _TOTAL_ entries",
-					searchPlaceholder: "Search"
-				},
-				dom: 'Bfrtip',
-				buttons: [
-				'copy', 'csv', 'pdf', 'print'
-				]
-			});
-			var table = $('.select-row').DataTable();
-			$('.select-row tbody').on('click', 'tr', function () {
-				if ($(this).hasClass('selected')) {
-					$(this).removeClass('selected');
-				}
-				else {
-					table.$('tr.selected').removeClass('selected');
-					$(this).addClass('selected');
-				}
-			});
-			var multipletable = $('.multiple-select-row').DataTable();
-			$('.multiple-select-row tbody').on('click', 'tr', function () {
-				$(this).toggleClass('selected');
-			});
-		});
-	</script>
+	
 </body>
 </html>
